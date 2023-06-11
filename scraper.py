ed7bd4b110aa301ec4a5d74c194a36debbd6b034
lilac011298 <qandeel.arshad1@gmail.com>
2023-06-11 20:49:46 +0500

    removed unnecessary code

--- scraper.py
+++ scraper.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 try: 
-    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
     url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
 
     # res (response) contains HTML source code of the webpage
